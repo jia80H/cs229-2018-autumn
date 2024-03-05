d41003863d5fcc533a26128f15cdf94b629090e3
mlfy <mlfy_jia@outlook.com>
2024-03-05 08:14:46 +0800

    python tutorial

--- problem-sets/PS1/src/p01b_logreg.py
+++ problem-sets/PS1/src/p01b_logreg.py
@@ -15,6 +15,10 @@ def main(train_path, eval_path, pred_path):
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
 
     # *** START CODE HERE ***
+    clf = LogisticRegression()
+    clf.fit(x_train, y_train)
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    clf.predict(x_eval)
     # *** END CODE HERE ***
 
 
@@ -35,6 +39,35 @@ class LogisticRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+        def h(theta, x):
+            return 1/(1 + np.exp(-theta*x))
+
+        def gradient(theta, x, y):
+            return -1 / m * np.dot(x.T, (y - h(theta, x)))
+
+        def hessian(theta, x):
+            pass
+
+        def next_theta(theta, x, y):
+            m = np.linalg.inv(hessian(theta, x))*gradient(theta, x, y)
+            return theta - m
+        m, n = x.shape
+        print(n)
+        # Initialize theta
+        if self.theta is None:
+            self.theta = np.zeros(n)
+            print(self.theta)
+
+        # Update theta using Newton's Method
+        old_theta = self.theta
+        new_theta = next_theta(self.theta, x, y)
+        self.eps = 0.00001
+        while np.linalg.norm(new_theta - old_theta, 1) >= self.eps:
+            old_theta = new_theta
+            new_theta = next_theta(old_theta, x, y)
+
+        self.theta = new_theta
+
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -48,3 +81,9 @@ class LogisticRegression(LinearModel):
         """
         # *** START CODE HERE ***
         # *** END CODE HERE ***
+
+
+if __name__ == '__main__':
+    main(train_path='data/ds1_train.csv',
+         eval_path='data/ds1_valid.csv',
+         pred_path='preds/ds1_pred.txt')
